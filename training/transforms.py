0e916acdf3efa960148e7b3e45d796691a30968a
Paul <72379712+Paul-cloud24@users.noreply.github.com>
2026-04-07 15:08:03 +0200

    Delete training directory

--- training/transforms.py
+++
@@ -1,9 +0,0 @@
-from torchvision import transforms
-
-def get_train_transforms():
-    return transforms.Compose([
-        transforms.Resize((224,224)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(10),
-        transforms.ToTensor()
-    ])
